3414ea4c8021861b04f8a7ed976e0c3cc756897d
Marcos Casagrande <marcoscvp90@gmail.com>
2020-06-28 17:35:37 +0200

    Adapt to Deno - Oak

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
-'use strict'
-
 /**
  * Expose compositor.
  */
 
-module.exports = compose
 
 /**
  * Compose `middleware` returning
@@ -16,7 +13,7 @@ module.exports = compose
  * @api public
  */
 
-function compose (middleware) {
+export default function compose (middleware) {
   if (!Array.isArray(middleware)) throw new TypeError('Middleware stack must be an array!')
   for (const fn of middleware) {
     if (typeof fn !== 'function') throw new TypeError('Middleware must be composed of functions!')
